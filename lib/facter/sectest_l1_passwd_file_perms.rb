3692b8b3a2ae365492a623c265e3d7072bcbf759
Kai Pak <kai@kaipak.org>
2016-07-20 11:05:49 -0700

    Passwd file

---
+++ lib/facter/sectest_l1_passwd_file_perms.rb
@@ -0,0 +1,11 @@
+Facter.add('sectest_l1_passwd_file_root_owner') do
+  confine :kernel => 'Linux'
+  setcode do
+    passwd_file_perms = Facter::Core::Execution.exec("/bin/stat -c %a /etc/passwd")
+    if passwd_file_owner = /644/
+      'pass'
+    else
+      'fail'
+    end
+  end
+end
---
+++ lib/facter/sectest_l1_passwd_file_root_owner.rb
@@ -0,0 +1,11 @@
+Facter.add('sectest_l1_passwd_file_root_owner') do
+  confine :kernel => 'Linux'
+  setcode do
+    passwd_file_owner = Facter::Core::Execution.exec("/bin/stat -c %U /etc/passwd")
+    if passwd_file_owner = /root/
+      'pass'
+    else
+      'fail'
+    end
+  end
+end
