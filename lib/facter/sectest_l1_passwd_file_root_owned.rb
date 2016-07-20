ce7a3a097428e0494e412b1a0a59a605de27daac
Kai Pak <kai@kaipak.org>
2016-07-20 11:30:25 -0700

    alsdfkjas

--- lib/facter/sectest_l1_passwd_file_root_owned.rb
+++ lib/facter/sectest_l1_passwd_file_root_owned.rb
@@ -1,7 +1,7 @@
 Facter.add('sectest_l1_passwd_file_root_owned') do
   confine :kernel => 'Linux'
   setcode do
-    passwd_file_owner = Facter::Core::Execution.exec("/bin/stat -c \%U /etc/passwd")
+    passwd_file_owner = Facter::Core::Execution.exec("/bin/stat -c %U /etc/passwd")
     if passwd_file_owner == /root/
       'pass'
     else
