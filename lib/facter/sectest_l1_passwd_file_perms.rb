b6e0e834bf372fbf240c139fa917c8a8ae3923b9
Kai Pak <kai@kaipak.org>
2016-07-20 11:24:01 -0700

    escaped special chars

--- lib/facter/sectest_l1_passwd_file_perms.rb
+++ lib/facter/sectest_l1_passwd_file_perms.rb
@@ -1,7 +1,7 @@
 Facter.add('sectest_l1_passwd_file_root_owner') do
   confine :kernel => 'Linux'
   setcode do
-    passwd_file_perms = Facter::Core::Execution.exec("/bin/stat -c %a /etc/passwd")
+    passwd_file_perms = Facter::Core::Execution.exec("/bin/stat -c \%a /etc/passwd")
     if passwd_file_owner == /644/
       'pass'
     else
