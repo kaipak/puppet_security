e2cc082556c102ddfbc9bf1fadbd46b5576be934
Kai Pak <kai@kaipak.org>
2016-07-20 10:24:14 -0700

    end end end

--- lib/facter/sectest_l1_openssl_vers.rb
+++ lib/facter/sectest_l1_openssl_vers.rb
@@ -2,10 +2,10 @@ Facter.add('sectest_l1_openssl_vers') do
   confine :kernel => 'Linux'
   setcode do
     openssl_vers = Facter::Core::Execution.exec("/bin/openssl version | /bin/awk '{print $2}'")
-  end
-  if openssl_vers =~ /1.0.1/
-    'pass'
-  else
-    'fail'
+    if openssl_vers =~ /1.0.1/
+      'pass'
+    else
+      'fail'
+    end
   end
 end
