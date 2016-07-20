51de77a675995ed02ae930062cdd6ebf48ad0a2f
Kai Pak <kai@kaipak.org>
2016-07-20 11:32:55 -0700

    string not match

--- lib/facter/sectest_l1_passwd_file_perms.rb
+++ lib/facter/sectest_l1_passwd_file_perms.rb
@@ -2,7 +2,7 @@ Facter.add('sectest_l1_passwd_file_perms') do
   confine :kernel => 'Linux'
   setcode do
     passwd_file_perms = Facter::Core::Execution.exec("/bin/stat -c %a /etc/passwd")
-    if passwd_file_perms == /644/
+    if passwd_file_perms == '644'
       'pass'
     else
       'fail'
